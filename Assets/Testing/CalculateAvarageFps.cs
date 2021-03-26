eeca02d5a19535700d7292475dc0200814622451
Sonatai <gamedevsonatai@yahoo.com>
2021-03-26 17:57:29 +0100

    FPS Calculation

---
+++ Assets/Testing/CalculateAvarageFps.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculateAvarageFps : MonoBehaviour
+{
+    float avg    = 0f;
+    public float tenSec = 10;
+    bool timerRunning = false;
+    public bool reset = false;
+    
+    // https://answers.unity.com/questions/1040630/do-something-for-10-sec.html
+    void Update()
+    {
+        if (reset)
+        { 
+            tenSec = 10;
+            timerRunning = true;
+            reset = false;
+        }
+        
+        if(timerRunning){
+            tenSec -= Time.smoothDeltaTime;
+            if(tenSec >= 0){  
+                avg += ((Time.deltaTime / Time.timeScale) - avg) * 0.03f;
+            }else{
+                Debug.Log(1f / avg);
+                timerRunning = false;
+            }
+        }
+    }
+}
