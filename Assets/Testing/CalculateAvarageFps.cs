10397c5efca6336eb63afd0ad173d14a28841219
Sonatai <gamedevsonatai@yahoo.com>
2021-04-11 22:00:42 +0200

    Fps calculater angepasst

--- Assets/Testing/CalculateAvarageFps.cs
+++ Assets/Testing/CalculateAvarageFps.cs
@@ -1,34 +1,54 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class CalculateAvarageFps : MonoBehaviour
 {
-    float avg    = 0f;
-    public float tenSec = 10;
+    private float tenSecStatic = 1f;
+    private float totalSecStatic;
+    float avg = 0f;
+    private float tenSec;
+    public float totalSec = 60;
     bool timerRunning = false;
     public bool reset = false;
     public string nameSpace;
     public string positon;
     public string focuspunkt;
-    
-    // https://answers.unity.com/questions/1040630/do-something-for-10-sec.html
+
+    // https://answers.unity.com/questions/326621/how-to-calculate-an-average-fps.html
+
+    private void Start()
+    {
+        totalSecStatic = totalSec;
+    }
+
     void Update()
     {
         if (reset)
-        { 
-            tenSec = 10;
-            avg    = 0f;
+        {
+            tenSec = tenSecStatic;
+            totalSec = totalSecStatic;
+            avg = 0f;
             timerRunning = true;
             reset = false;
         }
-        
-        if(timerRunning){
+
+        if (timerRunning)
+        {
             tenSec -= Time.smoothDeltaTime;
-            if(tenSec >= 0){  
+            totalSec -= Time.smoothDeltaTime;
+            if (totalSec >= 0)
+            {
                 avg += ((Time.deltaTime / Time.timeScale) - avg) * 0.03f;
-            }else{
+            }
+            else
+            {
+                timerRunning = false;
+            }
+            if (tenSec < 0)
+            {
                 Debug.Log($"{nameSpace} - position {positon} - focuspoint {focuspunkt}");
                 Debug.Log(1f / avg);
-                timerRunning = false;
+                tenSec = tenSecStatic;
             }
         }
     }
